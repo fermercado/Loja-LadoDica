c9338987857b08ea4279a45e52d86cc805061fac
Fernando Mercado <fermercado@live.com>
2022-03-30 23:32:18 -0300

    add product list

---
+++ products.js
@@ -0,0 +1,194 @@
+let productsJson = [
+  {
+    id: 1,
+    name: 'Conjunto Abacate',
+    img: [
+      {
+        id: 101,
+        src: 'img/ABACATE-1.jpg'
+      },
+      {
+        id: 102,
+        src: 'img/ABACATE-3.jpg'
+      },
+      {
+        id: 103,
+        src: 'img/ABACATE-4.jpg'
+      }
+    ],
+    price: 99.9,
+    sizes: ['P', 'M', 'G'],
+    description:
+      'Kit Abacate é composto por body + fraldinha de boca + sapatinhos de tricô'
+  },
+  {
+    id: 2,
+    name: 'Conjunto Milk e Cookie',
+    img: [
+      {
+        id: 202,
+        src: 'img/MILK-1.jpg'
+      },
+      {
+        id: 203,
+        src: 'img/MILK-2.jpg'
+      },
+      {
+        id: 204,
+        src: 'img/MILK-3.jpg'
+      }
+    ],
+    price: 99.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Combinadinho composto por body + fralda de boca + sapatinho'
+  },
+  {
+    id: 3,
+    name: 'Conjunto Pascoa ',
+    img: [
+      {
+        id: 303,
+        src: 'img/pascoa2.jpg'
+      },
+      {
+        id: 304,
+        src: 'img/pascoa3.jpg'
+      },
+      {
+        id: 305,
+        src: 'img/pascoa.jpg'
+      }
+    ],
+    price: 59.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Body de pascoa coelhinha com tutu, super fofo'
+  },
+  {
+    id: 4,
+    name: 'Body Halloween  ',
+    img: [
+      {
+        id: 404,
+        src: 'img/dia das bruxas 1.jpg'
+      },
+      {
+        id: 405,
+        src: 'img/dia das bruxas 3.jpg'
+      },
+      {
+        id: 406,
+        src: 'img/dia das bruxas 4.jpg'
+      }
+    ],
+    price: 59.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Body Halloween'
+  },
+
+  {
+    id: 6,
+    name: 'Body Batman Baby ',
+    img: [
+      {
+        id: 604,
+        src: 'img/Batman 1.jpg'
+      },
+      {
+        id: 605,
+        src: 'img/Batman 2.jpg'
+      },
+      {
+        id: 606,
+        src: 'img/Batman 4.jpg'
+      }
+    ],
+    price: 64.9,
+    sizes: ['P', 'M', 'G'],
+    description:
+      'Roupinha perfeita pra seu pequeno herói sair por aí espalhando fofura!'
+  },
+  {
+    id: 7,
+    name: 'Conjunto Dinossauro ',
+    img: [
+      {
+        id: 704,
+        src: 'img/DINOBOY-1.jpg'
+      },
+      {
+        id: 705,
+        src: 'img/DINOBOY-2.jpg'
+      },
+      {
+        id: 706,
+        src: 'img/DINOBOY-3.jpg'
+      }
+    ],
+    price: 99.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Conjunto Body + Fralda de boca + Sapatinho'
+  },
+  {
+    id: 8,
+    name: 'Conjunto Capitão America ',
+    img: [
+      {
+        id: 804,
+        src: 'img/kit capitao 2.jpg'
+      },
+      {
+        id: 805,
+        src: 'img/kit capitao 3.jpg'
+      },
+      {
+        id: 806,
+        src: 'img/kit capitao 1.jpg'
+      }
+    ],
+    price: 79.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Conjunto Body + Sapatinho - Capitão America'
+  },
+  {
+    id: 9,
+    name: 'Conjunto Meu Primeiro Natal ',
+    img: [
+      {
+        id: 904,
+        src: 'img/NATALSUSPFEM1.jpg'
+      },
+      {
+        id: 905,
+        src: 'img/NATALSUSPFEM2.jpg'
+      },
+      {
+        id: 906,
+        src: 'img/NATALSUSPFEM3.jpg'
+      }
+    ],
+    price: 129.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Conjunto Body + Shortinho suspensório'
+  },
+  {
+    id: 10,
+    name: 'Conjunto Super Mário ',
+    img: [
+      {
+        id: 1004,
+        src: 'img/ROUPINHA SUPER MARIO - 1.jpg'
+      },
+      {
+        id: 1005,
+        src: 'img/ROUPINHA SUPER MARIO - 2.jpg'
+      },
+      {
+        id: 1006,
+        src: 'img/ROUPINHA SUPER MARIO - 3.jpg'
+      }
+    ],
+    price: 149.9,
+    sizes: ['P', 'M', 'G'],
+    description: 'Conjunto Body + Shortinho + Boiná'
+  }
+]
